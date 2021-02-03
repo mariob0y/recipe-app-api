76b05fd5ff75bd1fb466d176fe6ca7496461c6d4
mariob0y <suroviymarioboy@gmail.com>
2021-02-03 10:43:31 +0200

    Added custom user model and respective tests

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', 
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#----------------------------------------------------------------------------#
+AUTH_USER_MODEL = 'core.User'
